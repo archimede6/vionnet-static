2293647b8dffe50005e6a5f1d174fe258cd1e2bb
pesmar-a6 <m.pessina@archimede6.com>
2023-06-01 11:57:56 +0200

    added vionnet logo

--- script/script.js
+++ script/script.js
@@ -5,3 +5,38 @@ randomNumber = randomNumber*imagesLenght;
 randomNumber = Math.floor(randomNumber);
 let chosenImage = images [randomNumber];
 vionnetImages.src = chosenImage;
+
+var desktop = true;
+
+var hoverWrapper = document.getElementById("hoverWrapper")
+var imageVionnet = document.getElementById("vionnetImages")
+var logo = document.getElementById("vionnetLogo")
+var contactsWrapper = document.getElementById("contactsWrapper")
+
+
+
+
+    if (window.innerWidth < 989) {
+        console.log("mobile")
+        imageVionnet.addEventListener("click", function() {
+            imageVionnet.classList.add("hidden")
+            logo.classList.remove("hidden")
+            contactsWrapper.classList.remove("hidden")
+        })
+    } else {
+        console.log("desktop")
+        imageVionnet.addEventListener("mouseover", function() {
+            //console.log("mouseover")
+            imageVionnet.classList.add("hidden")
+            logo.classList.remove("hidden")
+            contactsWrapper.classList.remove("hidden")
+        })
+
+        hoverWrapper.addEventListener("mouseleave", function() {
+            //console.log("mouseover")
+            imageVionnet.classList.remove("hidden")
+            logo.classList.add("hidden")
+            contactsWrapper.classList.add("hidden")
+        })
+    }
+
